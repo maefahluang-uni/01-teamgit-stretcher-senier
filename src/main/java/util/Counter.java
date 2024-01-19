c1a1401aafad7233be5b526e86c42a536e1f74bd
PiyawatSaennang <157101330+PiyawatSaennang@users.noreply.github.com>
2024-01-19 08:29:43 +0000

    TestFeature1

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -30,12 +30,12 @@ public class Counter {
 
 	// TODO: dev1- method for increment to closest even number
 	public void incrementToEven() {
-		_ctr = -99;
+		_ctr +=(_ctr % 2 == 0) ? 2:1 ;
 	}
 
 	// TODO: dev1- method for decrement to closest even number
 	public void decrementToEven() {
-		_ctr = -99;
+		_ctr -=(_ctr % 2 == 0) ? 2:1;
 	}
 
 	// TODO: dev2- method for increment to closest prime number
